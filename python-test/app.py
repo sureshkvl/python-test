50120c9786bdaac67b8498d0c72c90fb2c379bd0
suresh <sureshkumarr.s@gmail.com>
2019-11-22 12:27:04 +0530

    initial commit

---
+++ python-test/app.py
@@ -0,0 +1,9 @@
+
+from my_sum import sum
+
+
+if __name__ == '__main__':
+    data = [1, 2, 3]
+    result = sum(data)
+    print(result)
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from setuptools import find_packages
+
+install_requires = [
+]
+
+test_requires = []
+
+setup(
+    name='python-test1',
+    version='0.1',
+    description="Python Test program",
+    author="suresh",
+    author_email="sureshkumarr.s@gmail.com",
+    url="https://github.com/sureshkvl/python-test",
+    packages=find_packages(exclude=['test']),
+    include_package_data=True,
+    install_requires=install_requires,
+    license="Apache",
+    keywords='python',
+    python_requires='>=2.6, <3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: User Interfaces',
+        'Programming Language :: Python :: 2.7'
+    ]
+)
