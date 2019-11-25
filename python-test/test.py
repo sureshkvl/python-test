d672696bbfbdc762aed22aa131175c8865184a2c
suresh <sureshkumarr.s@gmail.com>
2019-11-25 13:58:11 +0530

    r4

--- python-test/test.py
+++ python-test/test.py
@@ -10,7 +10,7 @@ class TestSum(unittest.TestCase):
         """
         data = [1, 2, 3]
         result = sum(data)
-        self.assertEqual(result, 7)
+        self.assertEqual(result, 6)
 
 if __name__ == '__main__':
     unittest.main()
