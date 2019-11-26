bcda3f8c76e0e8bf8d09890a0d8f28104295a5ae
suresh <sureshkumarr.s@gmail.com>
2019-11-26 11:02:06 +0530

    0.7.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ test_requires = []
 
 setup(
     name='python-test1',
-    version='0.7.3',
+    version='0.7.4',
     description="Python Test program",
     author="suresh",
     author_email="sureshkumarr.s@gmail.com",
